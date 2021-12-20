575bdf04a157ea21537f36eef63c5c9d55457e74
Nattzy <new.nattzy@gmail.com>
2021-12-20 14:32:26 +0300

    First pull request. Just test.

---
+++ Randomville/DataBase.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace Randomville
+{
+    internal class DataBase : DbContext
+    {
+
+    }
+}
--- Randomville/Items/Item.cs
+++ Randomville/Items/Item.cs
@@ -1,15 +1,13 @@
 ﻿using System;
-using System.Data.Entity;
 
 
 namespace Items
 {
-    public class Items : DbContext
+    public class Items
     {
         // TODO: Использовать коллекции и LINQ для хранения предметов в рюкзаке и их добавлении/поиске/удалении
 
         protected Items()
-            : base("DBConnectionString")
         {
             // 
         }
