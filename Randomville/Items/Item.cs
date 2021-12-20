17927d30c978c0dd9333ac8278dd0970e8eb65b6
Nattzy <new.nattzy@gmail.com>
2021-12-20 14:48:44 +0300

    Revert "First pull request. Just test."
    This reverts commit 575bdf04a157ea21537f36eef63c5c9d55457e74.

--- Randomville/Items/Item.cs
+++ Randomville/Items/Item.cs
@@ -1,13 +1,15 @@
 ﻿using System;
+using System.Data.Entity;
 
 
 namespace Items
 {
-    public class Items
+    public class Items : DbContext
     {
         // TODO: Использовать коллекции и LINQ для хранения предметов в рюкзаке и их добавлении/поиске/удалении
 
         protected Items()
+            : base("DBConnectionString")
         {
             // 
         }
